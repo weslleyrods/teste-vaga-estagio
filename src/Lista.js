3dc5d3662eee58a7650fa638f039130894a4590f
Weslley <weslley@pop-os.localdomain>
2022-09-11 16:09:22 -0300

    feat: manipula erro de requisicao dos dados para lista

--- src/Lista.js
+++ src/Lista.js
@@ -3,14 +3,26 @@ import {useState, useEffect} from 'react';
 
 const Lista = (props)=>{
     const [empresas, setEmpresas] = useState([]);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState([]);
 
     useEffect(()=>{
         fetch('http://localhost:8000/empresas')
         .then(res=>{
+            if(!res.ok){
+                throw Error('Não foi possível carregados os dados, tente novamente mais tarde.')
+            }
+
             return res.json();
         })
         .then(data=>{
             setEmpresas(data);
+            setIsPending(false);
+            setError([]); 
+        })
+        .catch((err)=>{
+            setIsPending(false); 
+            setError(err.message);
         })
     },[]);
 
@@ -23,6 +35,8 @@ const Lista = (props)=>{
                             <th>Nome da Empresa</th>
                             <th>Editar</th>                    
                         </tr>
+                    {error && <div>{error}</div>}
+                    {isPending && <div>Carregando...</div>}
                     </thead>
                     {empresas.map((empresa)=>(
                     <tbody >
